1529fb659157324b606e42de4aada3bf311a2780
Jullians1105 <111465889+Jullians1105@users.noreply.github.com>
2024-09-12 23:22:49 -0500

    Intento de conectar base de datos

--- app/src/main/java/com/example/loginaguazero/LoginActivity.java
+++ app/src/main/java/com/example/loginaguazero/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.loginaguazero;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.Connection;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText usuario,clave;
+    TextView lblregistrar;
+    Button btnregistrar;
+    Connection con;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+        usuario=(EditText) findViewById(R.id.txtusuario);
+        clave=(EditText) findViewById(R.id.txtcalve);
+        lblregistrar=(TextView) findViewById(R.id.lblregistrar);
+        btnregistrar=(Button) findViewById(R.id.btnregistrar);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
