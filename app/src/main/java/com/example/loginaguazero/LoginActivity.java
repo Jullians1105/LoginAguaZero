d9bdd2d0c63e96accd0b3622aa8beb14060389d1
Jullians1105 <111465889+Jullians1105@users.noreply.github.com>
2024-09-15 21:04:07 -0500

    se pueden volver a las actividades

--- app/src/main/java/com/example/loginaguazero/LoginActivity.java
+++ app/src/main/java/com/example/loginaguazero/LoginActivity.java
@@ -1,39 +1,22 @@
 package com.example.loginaguazero;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.sql.Connection;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText usuario,clave;
-    TextView lblregistrar;
-    Button btnregistrar;
-    Connection con;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.login);
 
-        usuario=(EditText) findViewById(R.id.txtusuario);
-        clave=(EditText) findViewById(R.id.txtcalve);
-        lblregistrar=(TextView) findViewById(R.id.lblregistrar);
-//        btnregistrar=(Button) findViewById(R.id.btnregistrar);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn = findViewById(R.id.btnregistrar);
+        btn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
         });
     }
 }
